ef00204b892e92162c5d031db530c24e16f07dd6
Raytem <Danik1320@yandex.by>
2022-02-17 22:34:24 +0300

    some code was added in NewClass2

--- NewClass2.java
+++ NewClass2.java
@@ -4,5 +4,9 @@ public class NewClass2 {
         public static void main(String[] args) {
 
             System.out.println("456");
+
+            System.out.println("456");
+            System.out.println("456");
+            System.out.println("456");
         }
 }
